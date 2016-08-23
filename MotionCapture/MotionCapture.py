0061fdf39613f872fa31ffc3834561eabc3ed5d5
LMitchell <lawrenceomitchell@gmail.com>
2016-08-23 19:44:59 +1000

    WIP

--- MotionCapture/MotionCapture.py
+++ MotionCapture/MotionCapture.py
@@ -6,6 +6,7 @@ print ("Hello")
 print cv2.__version__
 
 MED_FILT = 5
+UPDATE_RATE = 500 #Update rate in milliseconds.
 
 #Snap the first image to pre-load.
 cap = cv2.VideoCapture(0)
@@ -17,6 +18,7 @@ cv2.imshow('Gray',gray)
 while(1):
 	ret ,frame = cap.read()
 	if ret==True:
+		#get the latest image.
 		last_gray = gray_med
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		gray_med = cv2.medianBlur(gray,MED_FILT)
@@ -25,7 +27,12 @@ while(1):
 		#Get the subtracted image.
 		diff = cv2.subtract(gray, last_gray)
 		cv2.imshow('Diff', diff)
-		k = cv2.waitKey(500) & 0xff
+		
+		#Threshold
+		ret, bin_diff = cv2.threshold(diff, 20, 255, cv2.THRESH_BINARY)
+		cv2.imshow('BinDiff', bin_diff)
+		
+		k = cv2.waitKey(UPDATE_RATE) & 0xff
 		if k == ord('q'):
 			print ("Exiting")
 			break
